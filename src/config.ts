ddd8015b9a42da49ae99f179f5c561ca9a8bb927
gintil <searcher64@gmail.com>
2021-11-01 07:55:54 -0400

    Rename initialize-commands to deploy-commands

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString, validateSync } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, validateSync } from 'class-validator';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,6 +11,10 @@ class Config {
   @IsString()
   @IsNotEmpty()
   BOT_CLIENT_ID = process.env.BOT_CLIENT_ID as string;
+
+  @IsString()
+  @IsOptional()
+  TESTING_GUILD_ID = process.env.TESTING_GUILD_ID as string;
 }
 
 const config = new Config();
